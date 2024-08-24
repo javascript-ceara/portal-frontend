96f4bcf8dd59e9aff300b79a550e28a0f565a357
Matheus França <115188878+FalaAiMatheus@users.noreply.github.com>
2024-08-24 15:48:41 -0300

    feat: View upcoming event talks (#17)

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,15 +1,29 @@
-"use client";
-
-import { User2Icon } from "lucide-react";
+import * as Avatar from "@/components/avatar";
 import { Button } from "@/components/button";
+import * as EventCard from "@/components/event-card";
 import { Hero } from "@/components/hero";
+import * as Popover from "@/components/popover";
 import * as Section from "@/components/section";
 import * as Typography from "@/components/typography";
-import * as Avatar from "@/components/avatar";
-import * as Popover from "@/components/popover";
-import * as EventCard from "@/components/event-card";
-import * as Dialog from "@/components/dialog";
-export default function Home() {
+import { createClient } from "@/services/supabase/server";
+import { User2Icon } from "lucide-react";
+
+export default async function Home() {
+  const client = await createClient();
+  const { data } = await client.from("presentations").select(`
+    id,
+    title,
+    description,
+    presentation_authors (
+      profile_id (
+        id,
+        full_name,
+        avatar_url,
+        name,
+        role
+      )
+    )
+  `);
   return (
     <main>
       <Hero />
@@ -31,51 +45,57 @@ export default function Home() {
             <div className="space-y-8">
               <Typography.TypographyH4>Agenda</Typography.TypographyH4>
               <ul className="divide-y divide-border">
-                {Array.from(Array(5).keys()).map((e) => {
-                  return (
-                    <li
-                      key={e}
-                      className="space-y-3 [&:not(:first-child)]:pt-4 [&:not(:last-child)]:pb-4"
-                    >
-                      <Popover.Root>
-                        <p className="flex items-center space-x-2 font-medium">
-                          <Popover.Trigger>
-                            <Avatar.Root className="flex h-8 w-8">
-                              <Avatar.Image src="https://avatars.githubusercontent.com/u/124599?v=4" />
+                {data?.map((presentation) => (
+                  <li
+                    key={presentation.id}
+                    className="space-y-3 [&:not(:first-child)]:pt-4 [&:not(:last-child)]:pb-4"
+                  >
+                    <Popover.Root>
+                      {presentation.presentation_authors.map((author, index) => (
+                        <div key={index}>
+                          <p className="flex items-center space-x-2 font-medium">
+                            <Popover.Trigger>
+                              <Avatar.Root className="flex h-8 w-8">
+                                <Avatar.Image
+                                  src={author.profile_id.avatar_url || "https://avatars.githubusercontent.com/u/124599?v=4"}
+                                />  
+                                <Avatar.Fallback>
+                                  <User2Icon className="h-8 w-8 rounded-full p-1 dark:text-foreground" />
+                                </Avatar.Fallback>
+                              </Avatar.Root>
+                            </Popover.Trigger>
+                            <span>{author.profile_id.full_name}</span>
+                          </p>
+                          <Popover.Content className="flex flex-col items-center">
+                            <Avatar.Root className="mb-4 flex h-24 w-24">
+                              <Avatar.Image
+                                src={
+                                  author.profile_id.avatar_url ||
+                                  "default-avatar-url"
+                                }
+                              />
                               <Avatar.Fallback>
                                 <User2Icon className="h-8 w-8 rounded-full p-1 dark:text-foreground" />
                               </Avatar.Fallback>
                             </Avatar.Root>
-                          </Popover.Trigger>
-                          <span>Cicero Viana</span>
-                        </p>
-                        <Popover.Content className="flex flex-col items-center">
-                          <Avatar.Root className="mb-4 flex h-24 w-24">
-                            <Avatar.Image src="https://avatars.githubusercontent.com/u/124599?v=4" />
-                            <Avatar.Fallback>
-                              <User2Icon className="h-8 w-8 rounded-full p-1 dark:text-foreground" />
-                            </Avatar.Fallback>
-                          </Avatar.Root>
-                          <Typography.TypographyH4>
-                            Cicero Viana
-                          </Typography.TypographyH4>
-                          <Typography.TypographySmall>
-                            Front-end developer
-                          </Typography.TypographySmall>
-                        </Popover.Content>
-                      </Popover.Root>
-
-                      <Typography.TypographyH3>
-                        Aplicando para vagas Internacionais
-                      </Typography.TypographyH3>
-                      <Typography.TypographyP className="max-w-3xl">
-                        Nessa apresentação irei compartilhar dicas e sugestões
-                        de dois recrutadores americanos, além de compartilhar o
-                        que aprendi ao aplicar a vagas internacionais.
-                      </Typography.TypographyP>
-                    </li>
-                  );
-                })}
+                            <Typography.TypographyH4>
+                              {author.profile_id.name}
+                            </Typography.TypographyH4>
+                            <Typography.TypographySmall>
+                              {author.profile_id.role}
+                            </Typography.TypographySmall>
+                          </Popover.Content>
+                        </div>
+                      ))}
+                    </Popover.Root>
+                    <Typography.TypographyH3>
+                      {presentation.title}
+                    </Typography.TypographyH3>
+                    <Typography.TypographyP className="max-w-3xl">
+                      {presentation.description}
+                    </Typography.TypographyP>
+                  </li>
+                ))}
               </ul>
             </div>
             <div className="flex flex-col gap-4 sm:flex-row">
