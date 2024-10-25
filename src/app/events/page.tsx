465159d505af8d876364bd43a84954bde9eb4a4e
Matheus França <115188878+MatheusFrancaDEV@users.noreply.github.com>
2024-10-25 09:55:45 -0300

    feat: add events in events page

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,11 +1,22 @@
 import { Section } from "@/components/section";
+import { createClient } from "@/services/supabase/server";
+import { EventPage } from "./event-page";
+
 export default async function Page() {
+  const client = createClient();
+
+  const { data: ourEvents } = await client
+    .from("events")
+    .select("*")
+    .order("start_date", { ascending: true });
+
   return (
     <Section>
       <Section.Container>
         <Section.Header>
           <Section.Title>Nossos eventos</Section.Title>
         </Section.Header>
+        <EventPage ourEvents={ourEvents} />
       </Section.Container>
     </Section>
   );
