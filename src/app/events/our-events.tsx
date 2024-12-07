cb150f84e7245378dfc291faca8837a09d4e9cb0
= <=>
2024-12-07 15:50:01 -0300

    feat: fixes event card on events page.

--- src/app/events/our-events.tsx
+++ src/app/events/our-events.tsx
@@ -13,18 +13,20 @@ export async function OurEvents({
   return (
     <>
       <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {ourEvents?.map(({ id, title, start_date, place, description }) => {
-          return (
-            <EventCard key={id}>
-              <EventCard.Title>
-                <Link href={`/events/${id}`}>{title}</Link>
-              </EventCard.Title>
-              <EventCard.StartDate date={start_date} />
-              <EventCard.Place>{place}</EventCard.Place>
-              <EventCard.Description>{description}</EventCard.Description>
-            </EventCard>
-          );
-        })}
+        {ourEvents?.map(
+          ({ id, title, start_date, place, address, description }) => {
+            return (
+              <EventCard key={id}>
+                <EventCard.Title>
+                  <Link href={`/events/${id}`}>{title}</Link>
+                </EventCard.Title>
+                <EventCard.StartDate date={start_date} />
+                <EventCard.PlaceAndAddress place={place} address={address} />
+                <EventCard.Description>{description}</EventCard.Description>
+              </EventCard>
+            );
+          },
+        )}
       </div>
       <Button className="mt-4 w-full xl:w-auto" variant={"outlined"}>
         Ver mais eventos
